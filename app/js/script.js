6db43c930527f1d2edd519215574dedc184708e8
Austen Turner <austenpturner@msn.com>
2024-05-02 11:45:42 -0700

    fix overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,13 @@
-const navToggle = document.querySelector('.nav__checkbox');
-const overlay = document.querySelector('.overlay');
-const body = document.querySelector('body');
+const navToggle = document.querySelector(".nav__checkbox");
+const overlay = document.querySelector(".overlay");
 
-navToggle.addEventListener('click', () => {
-    const shadowCast = overlay.getAttribute('data-visible');
-    if (shadowCast === 'false') {
-        overlay.setAttribute('data-visible', true);
-        body.classList.add('hidden')
-    } else if (shadowCast === 'true') {
-        overlay.setAttribute('data-visible', false);
-        body.classList.remove('hidden');
-    }
+navToggle.addEventListener("click", () => {
+  const shadowCast = overlay.getAttribute("data-visible");
+  if (shadowCast === "false") {
+    overlay.setAttribute("data-visible", true);
+    document.body.classList.add("hidden");
+  } else if (shadowCast === "true") {
+    overlay.setAttribute("data-visible", false);
+    document.body.classList.remove("hidden");
+  }
 });
